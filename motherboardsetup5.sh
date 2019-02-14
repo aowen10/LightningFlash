437768f1d4675176223f9852e5d362b12fe577e7
Alex Owen <aowen14@gmail.com>
2019-02-13 17:41:55 -0800

    add support for 5

---
+++ motherboardsetup5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#initial Motherboard setup
+
+sudo mkdir /mnt/hdd
+sudo mkdir /mnt/seed
+sudo mkdir /mnt/hdd/hdd1
+sudo mkdir /mnt/hdd/hdd2
+sudo mkdir /mnt/hdd/hdd3
+sudo mkdir /mnt/hdd/hdd4
+sudo mkdir /mnt/hdd/hdd5
