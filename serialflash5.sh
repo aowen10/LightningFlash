c0a16839a7f170963795ed6062a7dc8768e96ac8
Alex Owen <aowen14@gmail.com>
2019-02-19 00:37:55 -0500

    add serial flash for 5

---
+++ serialflash5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rsync -r --progress /mnt/seed/bitcoin-mainnet-data/ /mnt/hdd/hdd1/Docker_Volumes/Lightning/Mainnet/ 
+rsync -r --progress /mnt/seed/bitcoin-mainnet-data/ /mnt/hdd/hdd2/Docker_Volumes/Lightning/Mainnet/ 
+rsync -r --progress /mnt/seed/bitcoin-mainnet-data/ /mnt/hdd/hdd3/Docker_Volumes/Lightning/Mainnet/ 
+rsync -r --progress /mnt/seed/bitcoin-mainnet-data/ /mnt/hdd/hdd4/Docker_Volumes/Lightning/Mainnet/ 
+rsync -r --progress /mnt/seed/bitcoin-mainnet-data/ /mnt/hdd/hdd5/Docker_Volumes/Lightning/Mainnet/ 
