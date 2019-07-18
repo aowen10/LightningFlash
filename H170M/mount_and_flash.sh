c29ff552800e6a382f0eea817ee9ae6bd6b30d47
Alex Owen <aowen14@gmail.com>
2019-07-18 16:01:53 -0700

    Add H170M

---
+++ H170M/mount_and_flash.sh
@@ -0,0 +1,12 @@
+
+mount /dev/sda4 /mnt/hdd/hdd1
+mount /dev/sdb4 /mnt/hdd/hdd2
+mount /dev/sdc4 /mnt/hdd/hdd3
+mount /dev/sdd4 /mnt/hdd/hdd4
+mount /dev/sde4 /mnt/hdd/hdd5
+mount /dev/sdf4 /mnt/hdd/hdd6
+
+mount /dev/sdg /mnt/seed
+
+
+ls /mnt/hdd | parallel -v -j3 rsync -avzh --progress /mnt/seed/Docker_Volumes /mnt/hdd/{}/ 
