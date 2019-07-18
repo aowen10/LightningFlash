d2cfdbb8ef1c51b9ed6e19074794f4653b57f1d1
Alex Owen <aowen14@gmail.com>
2019-07-18 16:11:56 -0700

    edit cm folder

--- CM/mount_and_flash.sh
+++ CM/mount_and_flash.sh
@@ -2,7 +2,8 @@
 mount /dev/sda4 /mnt/hdd/hdd1
 mount /dev/sdb4 /mnt/hdd/hdd2
 mount /dev/sdc4 /mnt/hdd/hdd3
-mount /dev/sdd /mnt/seed
+mount /dev/sdd4 /mnt/hdd/hdd4
+mount /dev/sde /mnt/seed
 
 
 ls /mnt/hdd | parallel -v -j3 rsync -avzh --progress /mnt/seed/Docker_Volumes /mnt/hdd/{}/ 
--- CM/setup.sh
+++ CM/setup.sh
@@ -7,4 +7,5 @@ mkdir /mnt/hdd
 mkdir /mnt/hdd/hdd1
 mkdir /mnt/hdd/hdd2
 mkdir /mnt/hdd/hdd3
+mkdir /mnt/hdd/hdd4
 
